b7beccc16e08cd9b94ef05025afae7725edf0320
Pritesh Tailor <39673811+pritesh-tailor@users.noreply.github.com>
2023-07-30 14:11:47 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var memberSkills = document.getElementById("member-skills");
+
+    member.style.display = "none";
+    skills.style.display = "block";
+    memberSkills.style.display = "block";
+}
